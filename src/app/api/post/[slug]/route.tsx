0c8c087ca07e1a286e4d712a2cd38d8fb402edc8
Wayne Jones <2455598+Wayne-Jones@users.noreply.github.com>
2024-05-10 00:15:47 -0400

    Adding route to get individual post

--- src/app/api/post/[slug]/route.tsx
+++ src/app/api/post/[slug]/route.tsx
@@ -1,1 +1,32 @@
-export async function GET(request: Request) {}
+import { NextResponse } from "next/server";
+import { Post } from "@/helper/types";
+const { BLOG_URL, CONTENT_API_KEY } = process.env;
+
+//Get Individual Post
+export async function GET(request: Request, { params }: { params: { slug: string } }) {
+  const slug = params.slug;
+  if (BLOG_URL && CONTENT_API_KEY) {
+    try {
+      const url = `${BLOG_URL}/ghost/api/v3/content/posts/slug/${slug}/?key=${CONTENT_API_KEY}&fields=title,html,slug`;
+      const res = await fetch(url, { next: { revalidate: 10 } });
+      if (!res.ok) {
+        throw new Error("Unable to retreive from network");
+      }
+      const data: { posts: Post[] } = await res.json();
+      const post = data.posts[0];
+      return NextResponse.json(post, { status: 200 });
+    } catch (error) {
+      if (error instanceof Error) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+      } else {
+        // handle other errors
+        console.error(error);
+      }
+    }
+  } else {
+    return NextResponse.json(
+      { error: "Blog URL or Content API Key is missing" },
+      { status: 500 }
+    );
+  }
+}
--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,33 +1,45 @@
-import Link from 'next/link'
-import styles from '@/styles/Home.module.css'
-import { getPost, getAllPosts } from '../../../helper/util'
+import Link from "next/link";
+import styles from "@/styles/Home.module.css";
+import { getPost, getAllPosts } from "../../../helper/util";
+import { Post } from "@/helper/types";
+const { LOCAL_URL } = process.env;
 
 async function getData(slug: string) {
-    const posts = await getPost(slug);
-    return posts;
-  }
+  const res = await fetch(`${LOCAL_URL}/api/post/${slug}`, {
+    next: { revalidate: 10 },
+  });
+  const post: Post = await res.json();
+  return post;
+}
 
 export async function generateStaticParams() {
-    const posts = await getAllPosts();
-   
-    return posts.map((post) => ({
-      slug: post.slug,
-    }))
-  }
+  const res = await fetch(`${LOCAL_URL}/api/post`, {
+    next: { revalidate: 10 },
+  });
+  const posts: Post[] = await res.json();
 
-export default async function PostSlug({ params }: { params: { slug: string } }) {
-    const post = await getData(params.slug);
-    return (
-        <>
-            <div className={styles.container}>
-                <p className={styles.goback}>
-                    <Link href='/'>
-                        Go Back
-                    </Link>
-                </p>
-                <h1>{post.title}</h1>
-                {post.html && <div dangerouslySetInnerHTML={{ __html: post.html }}></div>}
-            </div>
-        </>
-    )
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
+}
+
+export default async function PostSlug({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const post = await getData(params.slug);
+  return (
+    <>
+      <div className={styles.container}>
+        <p className={styles.goback}>
+          <Link href="/">Go Back</Link>
+        </p>
+        <h1>{post.title}</h1>
+        {post.html && (
+          <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+        )}
+      </div>
+    </>
+  );
 }
