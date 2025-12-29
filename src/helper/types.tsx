948aeddd7074789d770e6553ea8c914bb941e653
Wayne Jones <waynejones@nyu.edu>
2025-12-28 22:21:34 -0500

    refactor(util): ♻️ Simplify API interactions and remove unused types
    Refactored the utility functions to utilize the GhostContentAPI directly for fetching posts. Removed the previously defined Post interface as it was no longer needed. This change streamlines the code and improves maintainability.
    
    - Integrated GhostContentAPI for fetching posts by tag and slug.
    - Removed the Post interface and related code.
    - Updated function implementations to use the new API methods.

--- src/helper/types.tsx
+++
@@ -1,5 +0,0 @@
-export interface Post {
-    title: string,
-    slug: string,
-    html?: string
-}
--- src/helper/util.tsx
+++ src/helper/util.tsx
@@ -1,51 +1,32 @@
-import { Post } from "./types";
-const { BLOG_URL, CONTENT_API_KEY } = process.env
+import GhostContentAPI from "@tryghost/content-api";
+const { BLOG_URL, CONTENT_API_KEY } = process.env;
 
-export async function getPost(slug: string): Promise<Post> {
-    let post: Post = {title: '', slug: ''};
-    if (BLOG_URL && CONTENT_API_KEY) {
-        const url = `${BLOG_URL}/ghost/api/v3/content/posts/slug/${slug}/?key=${CONTENT_API_KEY}&fields=title,html,slug`;
-        const res: {posts: Post[]} | void = await fetch(url)
-        .then<{ posts: Post[] }>((response: Response) => {
-            if (response.ok) {
-                return response.json()
-            }
-            else {
-                const message = `An error occured: ${response.status}`
-                throw new Error(message)
-            }
-        }).catch((error: Error) => {
-            throw new Error(error.message)
-        });
+const api = new GhostContentAPI({
+  url: BLOG_URL!,
+  key: CONTENT_API_KEY!,
+  version: "v5.0",
+});
 
-        const posts: Post[] = res.posts
-        post = posts[0]
-    }
-    return post;
+export async function getPostsByTag(tag: string) {
+  "use cache";
+
+  return await api.posts.browse({
+    filter: `tag:${tag}`,
+    include: ["tags", "authors"],
+    limit: "all",
+  });
 }
 
-export async function getAllPosts(): Promise<Post[]> {
-    // curl "https://demo.ghost.io/ghost/api/v3/content/posts/?key=22444f78447824223cefc48062"
+export async function getPost(slug: string) {
+  "use cache";
+  return await api.posts.read(
+    { slug: slug },
+    { include: ["tags", "authors"], formats: ["html", "plaintext"] }
+  );
+}
 
-    let posts: Post[] = [];
-    if (BLOG_URL && CONTENT_API_KEY) {
-        const url = `${BLOG_URL}/ghost/api/v3/content/posts/?key=${CONTENT_API_KEY}&fields=title,slug`
-        const res = await fetch(url, { next: { revalidate: 10 } })
-            .then<{ posts: Post[] }>((response: Response) => {
-                if (response.ok) {
-                    return response.json()
-                }
-                else {
-                    const message = `An error occured: ${response.status}`
-                    throw new Error(message)
-                }
-
-            }).catch((error: Error) => {
-                throw new Error(error.message)
-            });
-        if (res) {
-            posts = res.posts
-        }
-    }
-    return posts;
+export async function getAllPosts() {
+  "use cache";
+  // curl -H "Accept-Version: v6.0" "https://demo.ghost.io/ghost/api/content/posts/?key=22444f78447824223cefc48062"
+  return await api.posts.browse({ limit: "all", include: ["tags", "authors"] });
 }
